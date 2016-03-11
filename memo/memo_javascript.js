5d17cc19d915a824309a366cae2e8dfd6f979751
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-03-11 16:00:00 +0100

    Up!

--- memo/memo_javascript.js
+++ memo/memo_javascript.js
@@ -1,2 +1,14 @@
+// mécanisme de promesse
+var promise = new Promise(function(resolve, rejected){
+    console.log("Traitement numéro 1");
+    resolve();
+});
 
+promise.then(function(){
+    console.log("Traitement numéro 2");
+});
+
+promise.catch(function(){
+    console.log("Promesse non tenue");
+});
 
