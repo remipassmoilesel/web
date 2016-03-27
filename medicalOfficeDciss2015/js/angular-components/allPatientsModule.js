1788402306cdc8142f8514ca8770a78e31fe8fd0
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-03-27 12:47:36 +0200

    Up!

--- medicalOfficeDciss2015/js/angular-components/allPatientsModule.js
+++ medicalOfficeDciss2015/js/angular-components/allPatientsModule.js
@@ -4,9 +4,6 @@
  * @type
  */
 
-// récupérer le template et le stocker dans la variable template
-var template = require("../../templates/allPatientsModule.html");
-
 /**
  * Attend un module angular en paramètre, puis créé un composant et l'ajoute au module
  * @param {type} mod
@@ -21,14 +18,17 @@ module.exports = function (angularMod) {
     // Controlleur instancié à la création du composant
     var controller = function () {
         console.log("Nouveau controlleur créé pour displayAllPatients");
-        console.log(datahandler);
         this.allPatients = "Value";
     };
 
     // Créer un nouveau composant (camel case obligatoire)
     angularMod.component("allPatientsComponent", {
-        template: template,
         bindings: {},
-        controller: controller
+        controller: controller,
+        template: [
+            '<table>',
+            '<h3>Liste de tous les patients</h3>',
+            '{{$ctrl}}',
+            '</table>'].join('')
     });
 };
--- medicalOfficeDciss2015/js/functionnal-core/constants.js
+++ medicalOfficeDciss2015/js/functionnal-core/constants.js
@@ -2,21 +2,22 @@
  * Fichier contenant des constantes ou des variables de configuration
  */
 module.exports = {
-  /*
-   * Les différents fichiers de données
-   */
-  dataActes: "./data/actes.xml",
-  dataCabinet: "./data/cabinetInfirmier.xml",
-
-
-  logAll: function() {
-    console.log("Constantes: ");
-    for (var property in this) {
-
-      if (typeof this[property] !== "function") {
-        console.log("\t - " + property + ": " + this[property] + " - " + (typeof this[property]));
-      }
-
+    /*
+     * Les différents fichiers de données
+     */
+    dataActes: "./data/actes.xml",
+    dataCabinet: "./data/cabinetInfirmier.xml",
+    /**
+     * Afficher toutes les constantes et leurs types, hors fonctions.
+     * @returns {undefined}
+     */
+    logAll: function () {
+        console.log("Constantes: ");
+        
+        for (var property in this) {
+            if (typeof this[property] !== "function") {
+                console.log("\t - " + property + ": " + this[property] + " - " + (typeof this[property]));
+            }
+        }
     }
-  }
 };
